79271f0e71fcdcaf7a2311d2e12154e89da137a9
zeddted360 <nwiboazubuike@gmail.com>
2024-05-11 23:16:03 +0100

    used env for fire keys

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -3,13 +3,13 @@ import { getStorage } from 'firebase/storage';
 
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBAyN1UfZt_p6HAajmmXf9rRjfM0YRCm_Q',
-  authDomain: 'naija-memes-63822.firebaseapp.com',
-  projectId: 'naija-memes-63822',
-  storageBucket: 'naija-memes-63822.appspot.com',
-  messagingSenderId: '677790346204',
-  appId: '1:677790346204:web:e2ab7e4708a49d06c9bd19',
-  measurementId: 'G-FMCQVXH7EY',
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
